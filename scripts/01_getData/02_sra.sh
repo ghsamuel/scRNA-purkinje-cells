c4079a95959e33ad893924aba5d531073c021b02
Glady Samuel <hazitha@gmail.com>
2026-03-17 08:04:17 -0400

    Initial commit: project structure and download scripts

--- scripts/01_getData/02_sra.sh
+++ scripts/01_getData/02_sra.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=sra
+#SBATCH --job-name=sra_getfastqfiles
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH -c 12
-#SBATCH --mem=15G
+#SBATCH --mem=64G
 #SBATCH --partition=general
 #SBATCH --qos=general
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=first.last@uconn.edu
+#SBATCH --mail-user=jtz25002@uconn.edu
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
 
@@ -22,16 +22,22 @@ date
 module load parallel/20180122
 module load sratoolkit/3.0.1
 
-# The data are from this study, NCBI BioProject: PRJNA833256
-# Li, Q., Wang, M., Zhang, P. et al. A single-cell transcriptomic atlas tracking the neural basis of division of labour in an ant superorganism. Nat Ecol Evol 6, 1191–1204 (2022). https://doi.org/10.1038/s41559-022-01784-1
+# Dataset: GSE256438 - Purkinje cell heterogeneity in mouse cerebellum
+# Study: Khouri-Farah et al. (2025) Nature Neuroscience
+# FOXP genes regulate Purkinje cell diversity and cerebellar morphogenesis
+# BioProject: PRJNA1079673
 
 OUTDIR=../../data/fastq
-    mkdir -p ${OUTDIR}
+TMPDIR=../../tmp
+mkdir -p ${OUTDIR}
+mkdir -p ${TMPDIR}
 
 ACCLIST=../../metadata/subset.txt
 
+export TMPDIR=${TMPDIR}
+
 # use parallel to download 2 accessions at a time. 
-cat $ACCLIST | head -n 3 | parallel -j 3 "fasterq-dump -O ${OUTDIR} {}"
+cat $ACCLIST | parallel -j 3 "fasterq-dump -O ${OUTDIR} --split-files --temp ${TMPDIR} {}"
 
 # compress the files 
 ls ${OUTDIR}/*fastq | parallel -j 6 gzip
